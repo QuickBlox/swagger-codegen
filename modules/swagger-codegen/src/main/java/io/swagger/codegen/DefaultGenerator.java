65aeda321d9a2587199421935502fbefcc001411
Igor Khomenko <igor@quickblox.com>
2017-04-25 16:28:53 +0300

    logs

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultGenerator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultGenerator.java
@@ -649,8 +649,6 @@ public class DefaultGenerator extends AbstractGenerator implements Generator {
     @Override
     public List<File> generate() {
 
-        LOGGER.info("############ generate ############");
-
         if (swagger == null || config == null) {
             throw new RuntimeException("missing swagger input or config!");
         }
